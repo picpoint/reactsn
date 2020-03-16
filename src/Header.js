eecd95267b861e6f9042e127b40f34f32c546c65
picpoint <picpoint@yandex.ru>
2020-03-16 15:57:39 +0300

    template

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+	return(
+		<div className = "Header">
+			<ul>
+				<li>Home</li>
+				<li>Catalog</li>
+				<li>About...</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Header;
