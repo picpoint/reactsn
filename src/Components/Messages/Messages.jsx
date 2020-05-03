ac60c355d6288bd428d4e0ce6782c7b2ba4b7f94
picpoint <picpoint@yandex.ru>
2020-05-03 14:47:38 +0300

    make half page messages

--- src/Components/Messages/Messages.jsx
+++ src/Components/Messages/Messages.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import msg from './Messages.module.css';
+import { NavLink } from 'react-router-dom';
+
 
 function Messages() {
   return(
-    <div className="msgblock">
-      <span>Messages</span>
+    <div className={msg.messages}>
+      
+      <div className={msg.users}>
+        <NavLink to="/messages/1">Dmitry</NavLink>
+        <NavLink to="/messages/2">Semen</NavLink>
+        <NavLink to="/messages/3">Ivan</NavLink>
+        <NavLink to="/messages/4">Igor</NavLink>
+        <NavLink to="/messages/5">Petro</NavLink>
+      </div>
+      
+      <div className={msg.usrmsg}>  
+        <span>Hi</span>
+        <span>Hello</span>
+        <span>all good</span>
+        <span>so so</span>
+        <span>wats'up man</span>
+      </div>
+
     </div>
   );  
 }
