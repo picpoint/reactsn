b6ba2f076c88194ab9c199b033f266a935b4eb4a
picpoint <picpoint@yandex.ru>
2020-05-05 23:46:32 +0300

    work with components

--- src/Components/Messages/Messages.jsx
+++ src/Components/Messages/Messages.jsx
@@ -3,24 +3,57 @@ import msg from './Messages.module.css';
 import { NavLink } from 'react-router-dom';
 
 
+let datasUsers = [
+  {id: 1, name: "Dimych"},
+  {id: 2, name: "Semen"},
+  {id: 3, name: "Ivan"},
+  {id: 4, name: "Igor"},
+  {id: 5, name: "Petro"}
+];
+
+let datasMessages = [
+  {id: 1, dtmsg: "Hi"},
+  {id: 2, dtmsg: "Hello"},
+  {id: 3, dtmsg: "all fine"}
+];
+
+
+function SingleUser(props) {
+  return (    
+    <NavLink to={"/messages/" + props.id}>{props.name}</NavLink>    
+  );
+}
+
+
+
+function UserMessage(props) {
+  return(
+  <span>{props.message}</span>
+  );
+}
+
+
+
 function Messages() {
   return(
     <div className={msg.messages}>
       
       <div className={msg.users}>
-        <NavLink to="/messages/1">Dmitry</NavLink>
-        <NavLink to="/messages/2">Semen</NavLink>
-        <NavLink to="/messages/3">Ivan</NavLink>
-        <NavLink to="/messages/4">Igor</NavLink>
-        <NavLink to="/messages/5">Petro</NavLink>
+        <SingleUser id={datasUsers[0].id} name={datasUsers[0].name}/>
+        <SingleUser id={datasUsers[1].id} name={datasUsers[1].name}/>
+        {/* <SingleUser id="3" name="Ivan"/>        
+        <SingleUser id="4" name="Igor"/>
+        <SingleUser id="5" name="Petro"/>         */}
       </div>
       
       <div className={msg.usrmsg}>  
-        <span>Hi</span>
-        <span>Hello</span>
-        <span>all good</span>
-        <span>so so</span>
-        <span>wats'up man</span>
+        <UserMessage id={datasMessages[0].id} message={datasMessages[0].dtmsg}/>
+        <UserMessage id={datasMessages[1].id} message={datasMessages[1].dtmsg}/>
+        {/* <UserMessage message="Hi"/>
+        <UserMessage message="Hello"/>
+        <UserMessage message="all fine"/>
+        <UserMessage message="so so"/>
+        <UserMessage message="ok"/> */}
       </div>
 
     </div>
